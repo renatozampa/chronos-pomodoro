014e8ce565c59d62a54c3732c778402f9bf61556
Renato Zampa <renatomexzampa9@gmail.com>
2026-03-05 00:10:02 -0300

    Criação das primeiras páginas e a maioria dos primeiros componentes

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,79 @@
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
+import styles from './styles.module.css';
+import { useEffect, useState } from 'react';
+
+type AvailableThemes = 'dark' | 'light';
+
+export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem('theme') as AvailableThemes) || 'dark';
+
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault();
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  return (
+    <nav className={styles.menu}>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ir para a Home'
+        title='Ir para a Home'
+      >
+        <HouseIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ver histórico'
+        title='Ver histórico'
+      >
+        <HistoryIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ir para configurações'
+        title='Ir para configurações'
+      >
+        <SettingsIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Mudar tema'
+        title='Mudar tema'
+        onClick={event => handleThemeChange(event)}
+      >
+        {nextThemeIcon[theme]}
+      </a>
+    </nav>
+  );
+}
